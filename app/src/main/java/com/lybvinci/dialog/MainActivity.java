74cb823622d6c76919e25e30d2aefcd6e3e5f331
lybvinci <1018466460@qq.com>
2015-11-10 22:49:18 +0800

    dialog样式

--- app/src/main/java/com/lybvinci/dialog/MainActivity.java
+++ app/src/main/java/com/lybvinci/dialog/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
     public void showDialog(View view){
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view1 = inflater.inflate(R.layout.dialog,null);
+        Dialog dialog = new Dialog(this,R.style.Translucent_NoTitle);
+        dialog.addContentView(view1,new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        dialog.setContentView(view1);
+        dialog.show();
+    }
+
+    public void showWaitDialog(View view){
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = inflater.inflate(R.layout.dialog2,null);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.alpha);
+        view1.findViewById(R.id.iv).startAnimation(animation);
         Dialog dialog = new Dialog(this,R.style.Translucent_NoTitle);
         dialog.addContentView(view1,new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
